363d7583127e6174a56de959378350d130d9df33
Artem Yeremenko <a.yeremenko@asbis.com>
2024-09-26 14:00:40 +0300

    add resolver factory test

---
+++ tests/Unit/ResolverFactoryTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use ArtyomE\CreditCalculator\Enums\CalculationType;
+use ArtyomE\CreditCalculator\ResolverFactory;
+use ArtyomE\CreditCalculator\Resolvers\AnnuityResolver;
+use ArtyomE\CreditCalculator\Resolvers\DifferentiatedResolver;
+
+it('can create annuity resolver', function () {
+   $resolver = ResolverFactory::createResolver(CalculationType::annuity);
+   expect($resolver)->toBeInstanceOf(AnnuityResolver::class);
+});
+
+it('can create differentiated resolver', function () {
+    $resolver = ResolverFactory::createResolver(CalculationType::differentiated);
+    expect($resolver)->toBeInstanceOf(DifferentiatedResolver::class);
+});
